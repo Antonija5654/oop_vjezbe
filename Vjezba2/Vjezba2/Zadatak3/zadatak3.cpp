af88d24a67f87b1a9d7b89250fdbc89a3076b233
Antonija Klaic <antonijaklaic@hotmail.com>
2019-03-20 22:41:10 +0100

    vjezba2

--- Vjezba2/Vjezba2/Zadatak3/zadatak3.cpp
+++ Vjezba2/Vjezba2/Zadatak3/zadatak3.cpp
@@ -1,48 +1,58 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 typedef struct vektor {
 	int *niz;
 	int log, fiz;
 
-vektor *vector_new(vektor *v, int init)
+void vector_new(int init)
 {
-	vektor *v_novi = new vektor[init];
-	v_novi->log = 0;
-	v_novi->fiz = init;
-	return v_novi;
+	log = 0;
+	fiz = init;
+	niz = new int[init];
 }
 
-void vector_delete(vektor *v)
+void vector_delete()
 {
-	delete[] v->niz;
-	delete[] v;
+	delete[] niz;
 }
 
-vektor *vector_push_back(vektor *v)
+void vector_push_back()
 {
-	if (v->log == v->fiz)
+	if (log == fiz)
 	{
-		v = (vektor*)realloc(v, v->fiz * 2);
-		v->fiz = v->fiz * 2;
+		niz = (int*)realloc(niz, (fiz * 2)*sizeof(int));
+		fiz = fiz * 2;
 	}
 	int br;
 	cout << "unesite broj" << endl;
 	cin >> br;
-	v->niz[v->log] = br;
-	v->log++;
-	return v;
+	niz[log] = br;
+	log++;
 }
 
-int vector_pop_back(vektor *v)
+int vector_pop_back()
 {
-	v->log--;
-	return v->niz[v->log + 1];
+	if (log == 0)
+		return NULL;
+	log--;
+	return niz[log];
 }
 
-int &vector_front(vektor *v)
+int &vector_front()
 {
-	
+	return niz[0];
+}
+
+int &vector_back()
+{
+	return niz[log-1];
+}
+
+int vector_size()
+{
+	return log;
 }
 
 }vektor;
@@ -52,4 +62,26 @@ int main()
 	int init;
 	cout << "upisi velicinu niza"<<endl;
 	cin >> init;
+	vektor *v = new vektor;
+	v->vector_new(init);
+	cout << "push, pop, velicina, front, back, kraj" << endl;
+	string unos;
+	cin >> unos;
+	while (unos.compare("kraj") != 0)
+	{
+		if (unos.compare("push") == 0)
+			v->vector_push_back();
+		if (unos.compare("pop") == 0)
+			cout<<v->vector_pop_back()<<endl;
+		if (unos.compare("velicina") == 0)
+			cout << v->vector_size()<<endl;
+		if (unos.compare("front") == 0)
+			cout << v->vector_front()<<endl;
+		if (unos.compare("back") == 0)
+			cout << v->vector_back()<<endl;
+		cout << "push, pop, velicina, front, back, kraj" << endl;
+		cin >> unos;
+	}
+	delete v;
+	system("pause");
 }
